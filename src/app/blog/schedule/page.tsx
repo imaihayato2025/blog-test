f29506668b99651bab87ca29d9c81ee55aac76ba
imahaya <imaihayato2024@gmail.com>
2025-08-28 21:44:23 +0900

    0828

---
+++ src/app/blog/schedule/page.tsx
@@ -0,0 +1,21 @@
+import { getPostBySlug } from "../../lib/api";
+import Container from "../../../../components/Container";
+
+export default async function Schedule() {
+  // slug 固定で記事を取得
+  const slug = "schedule";
+  const post = await getPostBySlug(slug);
+
+  if (!post) {
+    return <div>記事が見つかりませんでした。</div>;
+  }
+
+  return (
+    <Container>
+      <h1>{post.title}</h1>
+      <p>公開日: {post.publishDate}</p>
+      <div dangerouslySetInnerHTML={{ __html: post.content }} />
+      {/* eyecatch や categories が必要ならここで表示 */}
+    </Container>
+  );
+}
---
+++ src/app/lib/api.tsx
@@ -0,0 +1,20 @@
+// src/lib/microcms.ts
+import { createClient } from "microcms-js-sdk"; // SDK から直接読み込む
+
+export const client = createClient({
+  serviceDomain: process.env.SERVICE_DOMAIN!,
+  apiKey: process.env.API_KEY!,
+});
+
+export async function getPostBySlug(slug: string) {
+  try {
+    const post = await client.get({
+      endpoint: "blogs",
+      queries: { filters: `slug[equals]${slug}` },
+    });
+    return post.contents[0];
+  } catch (err) {
+    console.log("~~ getPostBySlug~~", err);
+    console.log(err);
+  }
+}
